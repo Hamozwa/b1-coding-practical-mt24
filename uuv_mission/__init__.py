12c1b1da03ba68d92b225bffe77bbdac1e870ca5
Hamozwa <iamhanimoussa@gmail.com>
2024-10-24 21:38:48 +0100

    import sys, os

--- uuv_mission/__init__.py
+++
@@ -1,8 +0,0 @@
-from dynamic import Mission
-import os
-
-dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-csv_path = os.path.join(dir, 'data', 'mission.csv')
-
-mission = Mission.from_csv(csv_path)
-print(mission)
